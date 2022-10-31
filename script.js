56408e27200639e28815550cfa41d8a9dfd506ff
Fredrik Sloberg <sloberg@gmail.com>
2022-10-31 10:35:54 +0100

    allt

---
+++ script.js
@@ -0,0 +1,13 @@
+let about = document.querySelector('.about')
+let contact = document.querySelector('.contactnav')
+let hero = document.querySelector('.hero')
+
+
+// navbut.onclick = () =>{
+//     about.classList.toggle('active')
+// }
+navbut.onclick = () =>{
+    hero.classList.toggle('active')
+    // about.classList.toggle('active')
+    // contact.classList.toggle('active')
+}
